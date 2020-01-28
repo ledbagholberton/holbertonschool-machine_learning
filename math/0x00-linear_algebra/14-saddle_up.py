df9ed32eb16f0fadc291dd1ff965f14e0b9ea770
ledbagholberton <789@holbertonschool.com>
2020-01-28 23:48:46 +0000

    Adding comments to Point 14

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+""" Function which multiplies two matrix
+"""
+
 import numpy as np
 def np_matmul(mat1, mat2):
+    """Function np_matmul using Numpy"""
     if mat1.shape[1] != mat2.shape[0]:
         return(None)
     else:
