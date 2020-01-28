d7904758ea0eafa4fffb56960a7fd529614b48e7
ledbagholberton <789@holbertonschool.com>
2020-01-28 20:41:18 +0000

    Change the main 0

--- math/0x00-linear_algebra/0-slice_me_up.py
+++ math/0x00-linear_algebra/0-slice_me_up.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""
-Comments to fullfill the 8 lines
-..
-..
-"""
-main_0 = __import__('0-main')
-main_0
+arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
+arr1 = arr[0:2]
+arr2 = arr[-5, 10]
+arr3 = arr[1:6]
+print("The first two numbers of the array are: {}".format(arr1))
+print("The last five numbers of the array are: {}".format(arr2))
+print("The 2nd through 6th numbers of the array are: {}".format(arr3))
