f36a3f9ca3679746d92d73221a66e2558bacd5e1
ledbagholberton <789@holbertonschool.com>
2020-01-28 17:15:36 +0000

    Points 3 &4

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import numpy as np
+def matrix_shape(matrix):
+    a = np.array(matrix)
+    b = list(a.shape)
+    return(b)
---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import numpy as np
+def matrix_transpose(matrix):
+    a = np.array(matrix)
+    b = np.transpose(a)
+    return (b)
