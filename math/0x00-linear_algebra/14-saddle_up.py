64a9cda439fe7b2ce1a6a02ee7e77517caa39460
ledbagholberton <789@holbertonschool.com>
2020-01-28 23:52:44 +0000

    Changes in Style point 14

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -2,12 +2,11 @@
 """ Function which multiplies two matrix
 """
 
+
 import numpy as np
+
+
 def np_matmul(mat1, mat2):
     """Function np_matmul using Numpy"""
-    if mat1.shape[1] != mat2.shape[0]:
-        return(None)
-    else:
-        c = mat1.dot(mat2)
-        return(c)
-    
+    c = np.matmul(mat1, mat2)
+    return(c)
