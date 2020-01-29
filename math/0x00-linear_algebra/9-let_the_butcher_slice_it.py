96902eeb0a1f5c631575ee5504e84fe7f8ac8246
ledbagholberton <789@holbertonschool.com>
2020-01-29 01:08:05 +0000

    Fixing errors

--- math/0x00-linear_algebra/9-let_the_butcher_slice_it.py
+++ math/0x00-linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-"""FUnction which slice matrix 
-"""
-
 import numpy as np
 matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
                    [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]])
