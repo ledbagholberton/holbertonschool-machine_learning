0b703495298599c74c78a4df86755db6b43f90bb
ledbagholberton <789@holbertonschool.com>
2020-02-04 22:36:01 +0000

    New formula .... because ???

--- math/0x02-calculus/9-main.py
+++ math/0x02-calculus/9-main.py
@@ -2,5 +2,5 @@
 
 
 summation_i_squared = __import__('9-sum_total').summation_i_squared
-n = 3
+n = 1000
 print(summation_i_squared(n))
--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,6 +6,6 @@
 def summation_i_squared(n):
     """ FUnction summation """
     if type(n) == int and n > 0:
-        return (int(n*n*n/3 + n*n/2 + n/6))
+        return (int((n*(n+1)*(2*n + 1))/6))
     else:
         return (None)
