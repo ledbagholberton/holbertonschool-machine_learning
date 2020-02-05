8cc5abb1bc0a37fc19626b59107b154b7ae6f19f
ledbagholberton <789@holbertonschool.com>
2020-02-05 03:44:36 +0000

    Point 17 - Integral poly

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,13 +6,21 @@
 def poly_integral(poly, C=0):
     """function poly integral"""
     a = len(poly)
-    b = []
+    b = [C]
     try:
-        if type(poly) is list and a is not 0 and type(sum(poly)) is int:
-            for i in range(1, a):
-                b.append(i * poly[i])
-            return (b)
+        if type(poly) is not list:
+            return None
+        elif type(C) is not (int or float):
+            return None
+        elif a is 0:
+            return None
+        elif type(sum(poly)) is not (int or float):
+            return None
         else:
-            return(None)
+            for i in range(0, a):
+                b.append(poly[i] / (i + 1))
+            if len(set(b)) == 1:
+                return[0]
+            return (b)
     except TypeError:
         return(None)
