0aef29f6c46384dd67c29f5a2926f366592f0ed2
ledbagholberton <789@holbertonschool.com>
2020-02-05 04:04:48 +0000

    no try / except

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,22 +6,22 @@
 def poly_integral(poly, C=0):
     """function poly integral"""
     a = len(poly)
-    b = [C]
-    try:
-        if type(poly) is not list:
-            return None
-        elif type(C) is not (int or float):
-            return None
-        elif a is 0:
-            return None
-        elif type(sum(poly)) is not (int or float):
-            return None
-        else:
-            for i in range(0, a):
-                b.append(poly[i] / (i + 1))
-            if len(set(b)) == 1:
-                return[0]
-            return (b)
-    except TypeError:
-        return(None)
-        
+    b = []
+    if C is not 0:
+        b = [C]
+    if type(poly) is not list:
+        print("poly C is not list")
+        return None
+    elif type(C) is not (int or float):
+        print("C is not int or float")
+        return None
+    elif a is 0:
+        print("len poly is 0")
+        return None
+    elif type(sum(poly)) is not (int or float):
+        print("poly has an element not int or float")
+        return None
+    else:
+        for i in range(0, a):
+            b.append(poly[i] / (i + 1))
+        return (b)
--- math/0x02-calculus/17-main.py
+++ math/0x02-calculus/17-main.py
@@ -2,5 +2,5 @@
 
 poly_integral = __import__('17-integrate').poly_integral
 
-poly = [5, 3, 0, 1]
-print(poly_integral(poly, 5))
+poly = [2, 0, 12]
+print(poly_integral(poly, 1))
