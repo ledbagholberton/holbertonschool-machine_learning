fb0822ca7d6aa4a5ad40b61aeadf6a800c637897
ledbagholberton <789@holbertonschool.com>
2020-02-05 04:29:34 +0000

    fix issue with no try/except

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -7,18 +7,15 @@ def poly_derivative(poly):
     """function poly derivative """
     a = len(poly)
     b = []
-    try:
-        if type(poly) is not list:
-            return None
-        elif a is 0:
-            return None
-        elif type(sum(poly)) is not (int or float):
-            return None
-        else:
-            for i in range(1, a):
-                b.append(i * poly[i])
-            if len(set(b)) == 1:
-                return[0]
-            return (b)
-    except TypeError:
-        return(None)
+    if type(poly) is not list:
+        return None
+    elif a is 0:
+        return None
+    elif not all(isinstance(m, (int, float)) for m in poly):
+        return None
+    else:
+        for i in range(1, a):
+            b.append(i * poly[i])
+        if len(set(b)) == 1:
+            return[0]
+        return (b)
