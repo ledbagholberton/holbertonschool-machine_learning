0ed21ba0a4caf89b50526b4b97e79e4db8213c0d
ledbagholberton <789@holbertonschool.com>
2020-02-06 21:09:16 +0000

    Fix pep8 and comments

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-
+""" Poisson class
+"""
 import numpy as np
+
+
 class Poisson:
     def __init__(self, data=None, lambtha=1.):
+        """ Settings for class Poisson"""
         self.lambtha = float(lambtha)
         if data is None:
             if lambtha < 0:
@@ -13,8 +17,9 @@ class Poisson:
             if len(data) <= 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = sum(data)/len(data)
-    
+
     def pmf(self, k):
+        """Method pmf fr Poisson dist"""
         if type(k) is not int:
             k = int(k)
         if k < 0:
@@ -26,10 +31,11 @@ class Poisson:
             k_temp = k
             while k_temp > 0:
                 k_fact = k_temp * k_fact
-                k_temp = k_temp - 1 
+                k_temp = k_temp - 1
             return (a * b / k_fact)
 
     def cdf(self, k):
+        """Method CDF for Poisson Dist"""
         if type(k) is not int:
             k = int(k)
         if k < 0:
@@ -40,7 +46,7 @@ class Poisson:
             for i in range(0, k + 1, 1):
                 num = self.lambtha ** i
                 den = 1
-                for j in range (1, i + 1, 1):
+                for j in range(1, i + 1, 1):
                     den = den * j
                 d = num / den
                 suma = suma + d
