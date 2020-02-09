037c2716913a0365b13c6b455b8f9c0f452ff2e7
ledbagholberton <789@holbertonschool.com>
2020-02-09 04:18:05 +0000

    changing restriction in p

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -12,7 +12,7 @@ class Binomial:
         if data is None:
             if n <= 0:
                 raise ValueError("n must be a positive value")
-            if p < 0 or p > 1:
+            if p <= 0 or p >= 1:
                 raise ValueError("p must be greater than 0 and less than 1")
         else:
             if type(data) is not list:
