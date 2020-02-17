81b0b35e44de8935ec131e189abe5e284308e4f2
ledbagholberton <abdel.perez@gmail.com>
2020-02-17 09:11:37 -0500

    Point 17 -P rivate variables

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -24,7 +24,7 @@ class DeepNeuralNetwork:
         ly.insert(0, nx)
         for l in range(1, myL):
             if type(ly[l-1]) is not int or ly[(l-1)] < 0:
-                raise ValueError("layers must be a list of positive integers")
+                raise TypeError("layers must be a list of positive integers")
             temp = np.random.randn(ly[l], ly[l-1]) * (np.sqrt(2/ly[l-1]))
             self.weights['W'+str(l)] = temp
             self.weights['b'+str(l)] = np.zeros((ly[l], 1))
--- supervised_learning/0x00-binary_classification/17-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/17-deep_neural_network.py
@@ -4,7 +4,6 @@ with private instances attributes
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 class DeepNeuralNetwork:
@@ -17,44 +16,27 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if type(layers) is not list:
             raise TypeError("layers must be a list of positive integers")
-        if not all(x > 0 and type(x) is int for x in layers):
-            raise ValueError("layers must be a list of positive integers")
         self.__L = len(layers)
         self.__cache = {}
-        self.__weights = he_et_al(nx, layers)
+        self.__weights = {}
+        myL = self.__L + 1
+        ly = layers.copy()
+        ly.insert(0, nx)
+        for l in range(1, myL):
+            if type(ly[l-1]) is not int or ly[(l-1)] < 0:
+                raise TypeError("layers must be a list of positive integers")
+            temp = np.random.randn(ly[l], ly[l-1]) * (np.sqrt(2/ly[l-1]))
+            self.__weights['W'+str(l)] = temp
+            self.__weights['b'+str(l)] = np.zeros((ly[l], 1))
 
         @property
-        def weights(self):
-            return self.__weights
+        def L(self):
+            return self.__L
 
         @property
         def cache(self):
             return self.__cache
 
         @property
-        def L(self):
-            return self.__L
-
-
-def he_et_al(nx, ly):
-    """
-    Arguments:
-    layers: list containing the numbers of Layers and the number of nodes on
-    each layer.
-    Initialization of each weight is based on method from He.
-    Returns:
-    weights -- python dict containing your parameters "W1","b1".. "WL", "bL":
-                W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
-                b1 -- bias vector of shape (layers_dims[1], 1)
-                ...
-                WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
-                bL -- bias vector of shape (layers_dims[L], 1)
-    """
-
-    wg = {}
-    myL = len(ly) + 1
-    ly.insert(0, nx)
-    for l in range(1, myL):
-        wg['W' + str(l)] = np.random.randn(ly[l], ly[l-1])*(np.sqrt(2/ly[l-1]))
-        wg['b' + str(l)] = np.zeros((ly[l], 1))
-    return wg
+        def weights(self):
+            return self.__weights
