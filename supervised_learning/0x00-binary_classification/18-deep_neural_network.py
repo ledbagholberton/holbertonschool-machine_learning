704d37c64ba5612852b9cbdf839766c95d4a3c2a
ledbagholberton <abdel.perez@gmail.com>
2020-02-17 12:00:38 -0500

    Point 18

--- supervised_learning/0x00-binary_classification/18-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/18-deep_neural_network.py
@@ -4,7 +4,6 @@ with private instances attributes
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 class DeepNeuralNetwork:
@@ -17,57 +16,46 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if type(layers) is not list:
             raise TypeError("layers must be a list of positive integers")
-        if not all(x > 0 and type(x) is int for x in layers):
-            raise ValueError("layers must be a list of positive integers")
+        self.nx = nx
+        self.layers = layers
         self.__L = len(layers)
         self.__cache = {}
-        self.__weights = he_et_al(nx, layers)
+        self.__weights = {}
+        ly = layers.copy()
+        ly.insert(0, nx)
+        for l in range(1, self.__L + 1):
+            if type(ly[l-1]) is not int or ly[(l-1)] < 0:
+                raise TypeError("layers must be a list of positive integers")
+            temp = np.random.randn(ly[l], ly[l-1]) * (np.sqrt(2/ly[l-1]))
+            self.__weights['W'+str(l)] = temp
+            self.__weights['b'+str(l)] = np.zeros((ly[l], 1))
 
-        @property
-        def weights(self):
-            return self.__weights
+    @property
+    def L(self):
+        return self.__L
 
-        @property
-        def cache(self):
-            return self.__cache
+    @property
+    def cache(self):
+        return self.__cache
 
-        @property
-        def L(self):
-            return self.__L
+    @property
+    def weights(self):
+        return self.__weights
 
     def sigmoid(self, Z):
+        """Function sigmoid"""
         sigm = 1 / (1 + np.exp(-Z))
         return sigm
 
     def forward_prop(self, X):
-        A1 = np.matmul(self.__W1, X) + self.__b1
-        H1 = self.sigmoid(A1)
-        self.__A1 = H1
-        A2 = np.matmul(self.__W2, self.__A1) + self.__b2
-        H2 = self.sigmoid(A2)
-        self.__A2 = H2
-        return (self.__A1, self.__A2)
-
-
-def he_et_al(nx, ly):
-    """
-    Arguments:
-    layers: list containing the numbers of Layers and the number of nodes on
-    each layer.
-    Initialization of each weight is based on method from He.
-    Returns:
-    weights -- python dict containing your parameters "W1","b1".. "WL", "bL":
-                W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
-                b1 -- bias vector of shape (layers_dims[1], 1)
-                ...
-                WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
-                bL -- bias vector of shape (layers_dims[L], 1)
-    """
-
-    wg = {}
-    myL = len(ly) + 1
-    ly.insert(0, nx)
-    for l in range(1, myL):
-        wg['W' + str(l)] = np.random.randn(ly[l], ly[l-1])*(np.sqrt(2/ly[l-1]))
-        wg['b' + str(l)] = np.zeros((ly[l], 1))
-    return wg
+        """Function forward propoagation"""
+        for i in range(self.__L + 1):
+            if i == 0:
+                self.__cache['A0'] = X
+            else:
+                A_tmp = (np.matmul(self.__weights['W' + str(i)],
+                                   self.__cache['A' + str(i - 1)])
+                         + self.__weights['b' + str(i)])
+                H_tmp = self.sigmoid(A_tmp)
+                self.__cache['A' + str(i)] = H_tmp
+        return (self.__cache['A3'], self.__cache)
