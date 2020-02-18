bfb18c73e23b45508f11221989e7db862f165abe
ledbagholberton <abdel.perez@gmail.com>
2020-02-18 14:39:20 -0500

    .

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -24,7 +24,7 @@ class DeepNeuralNetwork:
         ly = layers.copy()
         ly.insert(0, nx)
         for l in range(1, self.L + 1):
-            if (type(ly[l-1]) is not int or ly[(l-1)] < 0):
+            if (type(ly[l-1]) is not int or ly[(l-1)] <= 0):
                 raise TypeError("layers must be a list of positive integers")
             else:
                 temp = np.random.randn(ly[l], ly[l-1]) * (np.sqrt(2/ly[l-1]))
