38453317692f86e6e51a91070f07152f5aef40af
ledbagholberton <789@holbertonschool.com>
2020-02-19 23:50:22 -0500

    coming back to origin fie

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -14,7 +14,7 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) is not list or len(layers) == 0:
+        if type(layers) is not list:
             raise TypeError("layers must be a list of positive integers")
         self.nx = nx
         self.layers = layers
@@ -23,12 +23,9 @@ class DeepNeuralNetwork:
         self.weights = {}
         ly = layers.copy()
         ly.insert(0, nx)
-        if (type(ly[self.L + 1]) != int or ly[self.L + 1] <= 0):
-            raise TypeError("layers must be a list of positive integers")
         for l in range(1, self.L + 1):
-            if (type(ly[l-1]) != int or ly[(l-1)] <= 0):
+            if type(ly[l-1]) is not int or ly[(l-1)] < 0:
                 raise TypeError("layers must be a list of positive integers")
-            else:
-                temp = np.random.randn(ly[l], ly[l-1]) * (np.sqrt(2/ly[l-1]))
-                self.weights['W'+str(l)] = temp
-                self.weights['b'+str(l)] = np.zeros((ly[l], 1))
+            temp = np.random.randn(ly[l], ly[l-1]) * (np.sqrt(2/ly[l-1]))
+            self.weights['W'+str(l)] = temp
+            self.weights['b'+str(l)] = np.zeros((ly[l], 1))
