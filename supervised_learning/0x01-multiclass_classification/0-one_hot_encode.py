170abb99d31ef70ed03608d417d28c3c137232ff
ledbagholberton <789@holbertonschool.com>
2020-02-20 15:53:57 -0500

    First commit

---
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Function One-hot encode"""
+import numpy as np
+
+def one_hot_encode(Y, classes):
+    """Function one-hot encode"""
+    print(len(Y))
+    matrix = np.zeros((classes, len(Y))
+    
+    for i in range(len(Y)):
+        pos = Y[i]
+        print pos
+    return (matrix)
