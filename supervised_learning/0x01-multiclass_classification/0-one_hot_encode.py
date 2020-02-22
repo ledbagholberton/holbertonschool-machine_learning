caac29a19e0f01f0922674610a406ac0085c4bc1
Abdel <789@holbertonschool.com>
2020-02-22 21:22:14 +0000

    retire a print test

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -5,7 +5,6 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """Function one-hot encode"""
-    print(len(Y))
     matrix = np.zeros((classes, len(Y)))
     for i in range(len(Y)):
         a = Y[i]
