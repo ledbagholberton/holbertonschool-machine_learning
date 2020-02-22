af0ba821ec4a14eef865658eb867d74c27d311b1
Abdel <789@holbertonschool.com>
2020-02-22 21:34:38 +0000

    adding restricition

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -6,7 +6,9 @@ import numpy as np
 def one_hot_encode(Y, classes):
     """Function one-hot encode"""
     matrix = np.zeros((classes, len(Y)))
-    if len(Y) is 0 or classes is 0:
+    if len(Y) < 1:
+        return (None)
+    if  classes <= 0:
         return (None)
     for i in range(len(Y)):
         a = Y[i]
