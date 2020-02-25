0a3d1774a46e7fc1c1f43b31c77bd96c197f030d
ledbagholberton <789@holbertonschool.com>
2020-02-25 20:45:45 +0000

    Point 1

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -6,5 +6,6 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """Function that creates a layer"""
     heetal = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    my_layer = tf.layers.Dense(units = n, activation=activation, kernel_initializer=heetal, name='layer')
+    my_layer = tf.layers.Dense(units=n, activation=activation, 
+                               kernel_initializer=heetal, name='layer')
     return(my_layer(prev))
