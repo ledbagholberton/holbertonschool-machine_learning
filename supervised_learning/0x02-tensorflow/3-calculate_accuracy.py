2a76cd3b20ce8c92fa3a01d13f3c7e64dd46aefe
ledbagholberton <789@holbertonschool.com>
2020-02-25 22:03:11 +0000

    Including argmax for y & y_pred

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -12,6 +12,6 @@ def calculate_accuracy(y, y_pred):
     y_pred is a tensor containing the network’s predictions
     Returns: a tensor containing the decimal accuracy of the prediction
     """
-    equality = tf.equal(y_pred, y)
+    equality = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
     return(accuracy)
