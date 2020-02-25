e375644b798d209bb15ef5351ea628a8ceb9973b
ledbagholberton <789@holbertonschool.com>
2020-02-25 13:08:42 +0000

    First commit

--- supervised_learning/0x01-multiclass_classification/test.py
+++ supervised_learning/0x01-multiclass_classification/test.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
 import numpy as np
-oh_encode = __import__('0-one_hot_encode').one_hot_encode
+Deep = __import__('2-deep_neural_network').DeepNeuralNetwork
+DeepTest = __import__('2-deep_neural_network_test').DeepNeuralNetwork
 
 np.random.seed(3)
-classes = np.random.randint(0, 20)
-m = np.random.randint(100, 200)
-Y = np.random.randint(0, classes, m)
+nx, m = np.random.randint(100, 200, 2).tolist()
+X = np.random.randn(nx, m)
+Y = np.random.randint(0, 2, (1, m))
+deep = Deep(nx, [3, 1])
+deep.train(X, Y, iterations=10, graph=False, verbose=False)
+deep.save('0-test.pkl')
+del deep
+deep = DeepTest.load('0-test.pkl')
 np.set_printoptions(threshold=np.inf)
-print(oh_encode(Y, 'classes'))
+print(deep.L)
+for k, v in sorted(deep.cache.items()):
+    print(k, v)
+for k, v in sorted(deep.weights.items()):
+    print(k, v)
+    
