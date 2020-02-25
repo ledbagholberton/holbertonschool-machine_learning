48536aa726d842c8def5463fcb0cb700271a1075
ledbagholberton <789@holbertonschool.com>
2020-02-25 20:39:59 +0000

    Point 1

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""FUnction create a layer """
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """Function that creates a layer"""
+    heetal = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    my_layer = tf.layers.Dense(units = n, activation=activation, kernel_initializer=heetal, name='layer')
+    return(my_layer(prev))
