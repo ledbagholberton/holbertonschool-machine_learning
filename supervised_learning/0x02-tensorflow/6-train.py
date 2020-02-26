7de8477e0a91aaf65ad1ad99795c829f4d8c34d1
ledbagholberton <789@holbertonschool.com>
2020-02-26 22:00:47 +0000

    Changes in graph / save and others

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -27,29 +27,34 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
     Returns: the path where the model was saved
     """
 
-    graph = tf.Graph()
-    with graph.as_default():
-        x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-        y_pred = forward_prop(x, layer_sizes, activations)
-        accuracy = calculate_accuracy(y, y_pred)
-        loss = calculate_loss(y, y_pred)
-        train_op = create_train_op(loss, alpha)
-        tf.add_to_collection("my_collection", [x, y, y_pred, accuracy, loss])
-
+    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    tf.add_to_collection("x", x)
+    tf.add_to_collection("y", y)
+    y_pred = forward_prop(x, layer_sizes, activations)
+    tf.add_to_collection("y_pred", y_pred)
+    accuracy = calculate_accuracy(y, y_pred)
+    tf.add_to_collection("accuracy", accuracy)
+    loss = calculate_loss(y, y_pred)
+    tf.add_to_collection("loss", loss)
+    train_op = create_train_op(loss, alpha)
     saver = tf.train.Saver()
-    with tf.Session(graph=graph) as session:
+    init = tf.global_variables_initializer()
+    with tf.Session() as session:
+        session.run(init)
+        counter = 0
         for i in range(iterations):
-            acc_t = session.run(accuracy, feed_dict={X_train, Y_train})
-            acc_v = session.run(accuracy, feed_dict={X_valid, Y_valid})
-            loss_t = session.run(loss, feed_dict={X_train, Y_train})
-            loss_v = session.run(loss, feed_dict={X_valid, Y_valid})
-            session.run(train_op, feed_dict={X_train, Y_train})
+            acc_t = session.run(accuracy, feed_dict={x:X_train, y:Y_train})
+            loss_t = session.run(loss, feed_dict={x:X_train, y:Y_train})
+            acc_v = session.run(accuracy, feed_dict={x:X_valid, y:Y_valid})
+            loss_v = session.run(loss, feed_dict={x:X_valid, y:Y_valid})
             counter = counter + 1
             if (i == 0) or (counter == 100) or (i == iterations - 1):
-                print("After {i} iterations:")
+                print("After {} iterations:".format(i))
                 print("Training Cost: {}".format(loss_t))
                 print("Training Accuracy: {}".format(acc_t))
                 print("Validation Cost: {}".format(loss_v))
                 print("Validation Accuracy: {}".format(acc_v))
                 counter = 0
+            session.run(train_op, feed_dict={x:X_train, y:Y_train})
         save_path = saver.save(session, save_path)
+    return(save_path)
