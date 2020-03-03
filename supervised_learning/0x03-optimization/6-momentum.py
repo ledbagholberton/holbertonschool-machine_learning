0ce89efb5f36e0c51422df031fc05c3a312c9657
ledbagholberton <789@holbertonschool.com>
2020-03-03 17:14:50 +0000

    pep8

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Function Momentum
-Creates the training operation for a neural network in 
+Creates the training operation for a neural network in
 tensorflow using the gradient descent with momentum optimization algorithm:
 loss is the loss of the network
 alpha is the learning rate
@@ -10,6 +10,7 @@ Returns: the momentum optimization operation
 
 
 import tensorflow as tf
+
 
 def create_momentum_op(loss, alpha, beta1):
     """Function Momentum"""
