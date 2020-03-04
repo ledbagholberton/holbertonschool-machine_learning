9fa4920904997ac48e490bca4bf32f372ab45542
ledbagholberton <789@holbertonschool.com>
2020-03-04 02:47:29 +0000

    point 13 y 14

---
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""  Function Batch Normalization
+that normalizes an unactivated output of a neural network
+using batch normalization:
+
+Z is a numpy.ndarray of shape (m, n) that should be normalized
+m is the number of data points
+n is the number of features in Z
+gamma is a numpy.ndarray of shape (1, n) containing the scales
+used for batch normalization
+beta is a numpy.ndarray of shape (1, n) containing the offsets
+used for batch normalization
+epsilon is a small number used to avoid division by zero
+Returns: the normalized Z matrix
+"""
+
+
+import tensorflow as tf
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """Function Batch Normalization"""
+    m = len(Z)
+    mean = np.mean(Z)
+    std = np.std(Z)
+    Z_norm = (Z - mean) / ((std + epsilon)**0.5)
+    return(gamma * Z_norm + beta)
