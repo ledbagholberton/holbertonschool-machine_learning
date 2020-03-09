abf114af429bc176009e1b617797a34d675cf78f
ledbagholberton <789@holbertonschool.com>
2020-03-09 02:53:35 +0000

    PEP8

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -2,9 +2,12 @@
 """ FUnction Sensitivity
 calculates the sensitivity for each class in a confusion matrix:
 
-confusion is a confusion numpy.ndarray of shape (classes, classes) where row indices represent the correct labels and column indices represent the predicted labels
+confusion is a confusion numpy.ndarray of shape (classes, classes) where row
+indices represent the correct labels and column indices represent the predicted
+labels
 classes is the number of classes
-Returns: a numpy.ndarray of shape (classes,) containing the sensitivity of each class
+Returns: a numpy.ndarray of shape (classes,) containing the sensitivity of
+each class
 """
 import numpy as np
 
