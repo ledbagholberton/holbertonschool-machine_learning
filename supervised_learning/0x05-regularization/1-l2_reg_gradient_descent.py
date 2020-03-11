b7d3be6b14649907a4853407e2bcdfb4675df1bb
ledbagholberton <789@holbertonschool.com>
2020-03-11 02:37:23 +0000

    Point 1

---
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+""" Function l2_gradient descent
+updates the weights and biases of a neural network using gradient descent
+with L2 regularization:
+Y is a one-hot numpy.ndarray of shape (classes, m) that contains the correct
+labels for the data
+classes is the number of classes
+m is the number of data points
+weights is a dictionary of the weights and biases of the neural network
+cache is a dictionary of the outputs of each layer of the neural network
+alpha is the learning rate
+lambtha is the L2 regularization parameter
+L is the number of layers of the network
+The neural network uses tanh activations on each layer except the last,
+which uses a softmax activation
+The weights and biases of the network should be updated in place
+"""
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """Function 12_reg_gradient_descent"""
+    activation = ['tanh', 'tanh', 'softmax']
+    dW = {}
+    dWT = {}
+    db = {}
+    dZ = {}
+    m = Y.shape[1]
+    wg = weights.copy()
+    posi = str(L)
+    dZ['dZ'+posi] = cache['A' + posi] - Y
+    db['db'+posi] = np.sum(dZ['dZ'+posi], axis=1, keepdims=True)/m
+    dW['dW'+posi] = np.matmul(cache['A'+str(L - 1)],
+                              dZ['dZ'+posi].T) / m
+    dWT['dWT'+posi] = dW['dW'+posi].T
+    weights['W'+posi] = (1+lambtha)*wg['W'+posi] - alpha*dWT['dWT'+posi]
+    weights['b'+posi] = (1+lambtha)*wg['b'+posi] - alpha*db['db'+posi]
+    for i in range(L - 1, 0, -1):
+        posl = str(i-1)
+        posm = str(i+1)
+        pos = str(i)
+        if activation[i] == 'tanh':
+            g_temp = 1 - cache['A'+pos]**2
+        else:
+            g_temp = cache['A'+pos] * (1 - cache['A'+pos])
+        dZ['dZ'+pos] = np.matmul(wg['W'+posm].T, dZ['dZ'+posm]) * g_temp
+        db['db'+pos] = np.sum(dZ['dZ'+pos], axis=1, keepdims=True)/m
+        dW['dW'+pos] = np.matmul(cache['A'+posl],
+                                 dZ['dZ'+pos].T) / m
+        dWT['dWT'+pos] = dW['dW'+pos].T
+        weights['W'+pos] = wg['W'+pos] - alpha*dWT['dWT'+pos]
+        weights['b'+pos] = wg['b'+pos] - alpha*db['db'+pos]
+    return(weights)
