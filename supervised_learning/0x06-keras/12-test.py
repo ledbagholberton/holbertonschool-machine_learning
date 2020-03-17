7799c9877deb7ad94e2930c434c10b1b54f6eddd
ledbagholberton <789@holbertonschool.com>
2020-03-17 04:33:54 +0000

    Point 12

--- supervised_learning/0x06-keras/12-test.py
+++ supervised_learning/0x06-keras/12-test.py
@@ -1,29 +1,16 @@
 #!/usr/bin/env python3
-""" Functions save & load config
- saves a model’s configuration in
-JSON format:
-network is the model whose configuration should be saved
-filename is the path of the file that the configuration should be saved to
-Returns: None
-loads a model with a specific configuration:
-filename is the path of the file containing the model’s configuration in
-JSON format
-Returns: the loaded model
+""" Functions test neural network
+network is the network model to test
+data is the input data to test the model with
+labels are the correct one-hot labels of data
+verbose is a boolean that determines if output should be printed
+during the testing process
+Returns: the loss and accuracy of the model with the testing data
 """
 import tensorflow.keras as K
 
 
-def save_config(network, filename):
-    """FUnction save_config"""
-    network_json = network.to_json()
-    with open(filename, "w") as json_file:
-        json_file.write(network_json)
-
-
-def load_config(filename):
-    """FUnction load_config"""
-    json_file = open(filename, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = K.models.model_from_json(loaded_model_json)
-    return(loaded_model)
+def test_model(network, data, labels, verbose=True):
+    """FUnction test neural network"""
+    scores = network.evaluate(data, labels, verbose=verbose)
+    return(scores)
