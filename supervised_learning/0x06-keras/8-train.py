5d6f4cb0986a33bc00953c1704efa188782f5e04
ledbagholberton <789@holbertonschool.com>
2020-03-17 02:59:43 +0000

    Point 8

--- supervised_learning/0x06-keras/8-train.py
+++ supervised_learning/0x06-keras/8-train.py
@@ -41,14 +41,18 @@ def train_model(network, data, labels, batch_size, epochs,
                 alpha=0.1, decay_rate=1, save_best=False, filepath=None,
                 verbose=True, shuffle=False):
     """ Function train_model"""
+    def schedule(epochs):
+        """Function support callback Learning Rate"""
+        lr_cb = alpha / (1 + decay_rate * epochs)
+        return lr_cb
     callbacks = []
     if early_stopping is True and validation_data is not None:
         early_cb = K.callbacks.EarlyStopping(patience=patience,
                                              monitor='val_loss')
         callbacks.append(early_cb)
     if learning_rate_decay is True and validation_data is not None:
-        lr_cb = K.callbacks.LearningRateScheduler(schedule(alpha, decay_rate,
-                                                  epochs), verbose=1)
+        lr_cb = K.callbacks.LearningRateScheduler(schedule,
+                                                  verbose=1)
         callbacks.append(lr_cb)
     if save_best is True:
         save_cb = K.callbacks.ModelCheckpoint(filepath=filepath,
@@ -60,8 +64,3 @@ def train_model(network, data, labels, batch_size, epochs,
                                   validation_data=validation_data,
                                   callbacks=callbacks)
     return (trained_network)
-
-
-def schedule(alpha, decay_rate, epochs):
-    """Function support callback Learning Rate"""
-    return (alpha / (1 + decay_rate * epochs))
