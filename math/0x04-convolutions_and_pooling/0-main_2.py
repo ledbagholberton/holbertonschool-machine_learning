bc6a127ef4ca2dc33db7b705a9ea3a44f7d71ee1
ledbagholberton <789@holbertonschool.com>
2020-03-20 23:14:23 +0000

    Point 2

---
+++ math/0x04-convolutions_and_pooling/0-main_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+convolve_grayscale_padding = __import__('2-convolve_grayscale_padding').convolve_grayscale_padding
+
+np.random.seed(2)
+m = np.random.randint(1000, 2000)
+h, w = np.random.randint(100, 200, 2).tolist()
+fh, fw = (np.random.randint(3, 10, 2)).tolist()
+ph, pw = (np.random.randint(1, 5, 2)).tolist()
+
+images = np.random.randint(0, 256, (m, h, w))
+kernel = np.random.randint(0, 10, (fh, fw))
+conv_ims = convolve_grayscale_padding(images, kernel, (ph, pw))
+print(conv_ims)
+print(conv_ims.shape)
