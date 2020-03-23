82e33ae964abc31bbda6bb1628d43e62cab29988
ledbagholberton <789@holbertonschool.com>
2020-03-23 23:05:51 +0000

    Point 1

--- supervised_learning/0x07-cnn/1-pool_forward.py
+++ supervised_learning/0x07-cnn/1-pool_forward.py
@@ -25,41 +25,30 @@ import numpy as np
 
 
 def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
-    """ Function Pool Forward"""
-    convolve_A = convolve(A_prev, W, padding, stride)
-    convolve_B = np.sum(convolve_A + b)
-    convolve_C = activation(convolve_A)
-    return(convolve_C)
-
-
-def convolve(X, W, padding, stride):
-    """Function convolution"""
-    m, h, w, c = X.shape
-    kh, kw, kc, nc = W.shape
+    """ Function Convolution Forward"""
+    m, h, w, c = A_prev.shape
+    kh = kernel_shape[0]
+    kw = kernel_shape[1]
     sh = stride[0]
     sw = stride[1]
-    ph, pw = 0, 0
-    if padding == 'same':
-        ph = int(((h - 1)*sh + kh - h)/2) + 1
-        pw = int(((w - 1)*sw + kw - w)/2) + 1
-    new_X = np.pad(X, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                   mode='constant', constant_values=0)
-    ch = int(np.floor(((h - kh + 2*ph) / sh) + 1))
-    cw = int(np.floor(((w - kw + 2*pw) / sw) + 1))
-    new_conv = np.zeros((m, ch, cw, nc))
-    m_o = np.arange(0, m)
+    ch = int(np.floor(((h - kh) / sh) + 1))
+    cw = int(np.floor(((w - kw) / sw) + 1))
+    new_conv = np.zeros((m, ch, cw, c))
+    m_only = np.arange(0, m)
     for row in range(ch):
         for col in range(cw):
-            for n_k in range(nc):
-                a = row*sh
-                b = row*sh + kh
-                c = col*sw
-                d = col*sw + kw
-                new_conv[m_o, row, col, n_k] = np.sum(np.multiply
-                                                      (new_X[m_o, a:b,
-                                                             c:d, ],
-                                                       W[:, :, :, n_k]),
-                                                      axis=(1, 2, 3))
+            a = row*sh
+            b = row*sh + kh
+            c = col*sw
+            d = col*sw + kw
+            if mode == 'max':
+                new_conv[m_only, row, col] = np.max(A_prev[m_only,
+                                                           a:b,
+                                                           c:d,
+                                                           ], axis=(1, 2))
+            else:
+                new_conv[m_only, row, col] = np.average(A_prev[m_only,
+                                                               a:b,
+                                                               c:d,
+                                                               ], axis=(1, 2))
     return(new_conv)
-
-
