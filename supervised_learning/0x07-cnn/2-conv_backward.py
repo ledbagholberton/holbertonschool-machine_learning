587b08270d9991f9fc994fff98c3f9d050ad0601
ledbagholberton <789@holbertonschool.com>
2020-03-24 21:29:11 +0000

    Point 5 Keras

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""" Function Pool Forward
-Write a function 
-that performs back propagation over a convolutional layer of a neural network:
+""" Function Conv Backward
+Write a function that performs back propagation over a convolutional
+layer of a neural network:
 
-dZ is a numpy.ndarray of shape (m, h_new, w_new, c_new) containing the partial
-derivatives with respect to the unactivated output of the convolutional layer
+dZ is a numpy.ndarray of shape (m, h_new, w_new, c_new) containing the
+partial derivatives with respect to the unactivated output of the convolutional
+layer
 m is the number of examples
 h_new is the height of the output
 w_new is the width of the output
@@ -14,13 +15,14 @@ output of the previous layer
 h_prev is the height of the previous layer
 w_prev is the width of the previous layer
 c_prev is the number of channels in the previous layer
-W is a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the kernels for
-the convolution
+W is a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the kernels
+for the convolution
 kh is the filter height
 kw is the filter width
 b is a numpy.ndarray of shape (1, 1, 1, c_new) containing the biases applied to
 the convolution
-padding is a string that is either same or valid, indicating the type of padding used
+padding is a string that is either same or valid, indicating the type of
+padding used
 stride is a tuple of (sh, sw) containing the strides for the convolution
 sh is the stride for the height
 sw is the stride for the width
---
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Write a function def lenet5(X): that builds a modified version of the LeNet-5 architecture
+using keras:
+
+X is a K.Input of shape (m, 28, 28, 1) containing the input images for the network
+m is the number of images
+The model should consist of the following layers in order:
+Convolutional layer with 6 kernels of shape 5x5 with same padding
+Max pooling layer with kernels of shape 2x2 with 2x2 strides
+Convolutional layer with 16 kernels of shape 5x5 with valid padding
+Max pooling layer with kernels of shape 2x2 with 2x2 strides
+Fully connected layer with 120 nodes
+Fully connected layer with 84 nodes
+Fully connected softmax output layer with 10 nodes
+All layers requiring initialization should initialize their kernels with the he_normal
+initialization method
+All hidden layers requiring activation should use the relu activation function
+you may import tensorflow.keras as K
+Returns: a K.Model compiled to use Adam optimization (with default hyperparameters) and
+accuracy metrics
+"""
+import tensorflow.keras as K
+
+
+def lenet5(x):
+    """Function lenet5"""
+    model = K.Sequential()
+    model.add(K.layers.Conv2D(filters=6, kernel_size=(5,5), padding='same', activation='relu'))
+    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
+    model.add(K.layers.Conv2D(filters=16, kernel_size=(5, 5), padding='valid', activation='relu'))
+    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
+    model.add(K.layers.Flatten())
+    model.add(K.layers.Dense(units=120, activation='relu'))
+    model.add(K.layers.Dense(units=84, activation='relu'))
+    model.add(K.layers.Dense(units=10, activation = 'softmax'))
+    model.compile(loss='categorical_crossentropy',
+                            optimizer=K.optimizers.Adam(),
+                            metrics=['accuracy'])
+    return(model)
