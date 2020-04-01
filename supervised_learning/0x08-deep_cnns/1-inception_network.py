c0941b47037c21400122030794f84ecc8e3ad7ac
ledbagholberton <789@holbertonschool.com>
2020-04-01 16:20:05 +0000

    Prueba MACU

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
-""" FUnction Inception Block
-Write a function : that builds the inception network
-as described in Going Deeper with Convolutions (2014):
+"""Deep Convolutional Architectures"""
 
-You can assume the input data will have shape (224, 224, 3)
-All convolutions inside and outside the inception block should use a rectified
-linear activation (ReLU)
-You may use inception_block = __import__('0-inception_block').inception_block
-Returns: the keras model
-"""
 import tensorflow.keras as K
 inception_block = __import__('0-inception_block').inception_block
 
