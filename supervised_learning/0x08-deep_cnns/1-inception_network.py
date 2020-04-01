844408025d94af4edbe67ecbe641b7b6deeb0a68
ledbagholberton <789@holbertonschool.com>
2020-04-01 16:22:53 +0000

    Prueba con header MACU y SW GP

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -6,72 +6,35 @@ inception_block = __import__('0-inception_block').inception_block
 
 
 def inception_network():
-    """inception network
-    Returns: keras inception model
-    """
-    activation = 'relu'
-    k_init = K.initializers.he_normal(seed=None)
-
+    """Function Inception Block"""
     X = K.Input(shape=(224, 224, 3))
-
-    l1 = K.layers.Conv2D(filters=64, kernel_size=7,
-                         strides=2,
-                         padding='same',
-                         activation=activation,
-                         kernel_initializer=k_init)(X)
-
-    l_pool1 = K.layers.MaxPooling2D(pool_size=[3, 3],
-                                    strides=2,
-                                    padding='same')(l1)
-
-    l2_1 = K.layers.Conv2D(filters=64, kernel_size=1,
-                           padding='same',
-                           activation=activation,
-                           kernel_initializer=k_init)(l_pool1)
-
-    l2 = K.layers.Conv2D(filters=192, kernel_size=3,
-                         padding='same',
-                         activation=activation,
-                         kernel_initializer=k_init)(l2_1)
-
-    l_pool2 = K.layers.MaxPooling2D(pool_size=[3, 3],
-                                    strides=2,
-                                    padding='same')(l2)
-
-    l3 = inception_block(l_pool2, [64, 96, 128, 16, 32, 32])
-
-    l4 = inception_block(l3, [128, 128, 192, 32, 96, 64])
-
-    l_pool3 = K.layers.MaxPooling2D(pool_size=[3, 3],
-                                    strides=2,
-                                    padding='same')(l4)
-
-    l5 = inception_block(l_pool3, [192, 96, 208, 16, 48, 64])
-
-    l6 = inception_block(l5, [160, 112, 224, 24, 64, 64])
-
-    l7 = inception_block(l6, [128, 128, 256, 24, 64, 64])
-
-    l8 = inception_block(l7, [112, 144, 288, 32, 64, 64])
-
-    l9 = inception_block(l8, [256, 160, 320, 32, 128, 128])
-
-    l_pool4 = K.layers.MaxPooling2D(pool_size=[3, 3],
-                                    strides=2,
-                                    padding='same')(l9)
-
-    l10 = inception_block(l_pool4, [256, 160, 320, 32, 128, 128])
-
-    l11 = inception_block(l10, [384, 192, 384, 48, 128, 128])
-
-    l_avg_pool = K.layers.AveragePooling2D(pool_size=[7, 7],
-                                           strides=7,
-                                           padding='same')(l11)
-
-    dropout = K.layers.Dropout(0.4)(l_avg_pool)
-
-    Y = K.layers.Dense(1000, activation='softmax',
-                       kernel_initializer=k_init)(dropout)
-
-    model = K.models.Model(inputs=X, outputs=Y)
-    return model
+    init = K.initializers.he_normal(seed=None)
+    conv1 = K.layers.Conv2D(64, (7, 7),  padding='same', strides=(2, 2),
+                            kernel_initializer=init, activation='relu')(X)
+    mp1 = K.layers.MaxPooling2D((3, 3), strides=(2, 2),
+                                padding='same')(conv1)
+    conv2 = K.layers.Conv2D(64, (1, 1),  padding='same',
+                            kernel_initializer=init, activation='relu')(mp1)
+    conv2_1 = K.layers.Conv2D(192, (3, 3),  padding='same',
+                              kernel_initializer=init, activation='relu')(conv2)
+    mp2 = K.layers.MaxPooling2D((3, 3), strides=(2, 2),
+                                padding='same')(conv2_1)
+    inc3a = inception_block(mp2, [64, 96, 128, 16, 32, 32])
+    inc3b = inception_block(inc3a, [128, 128, 192, 32, 96, 64])
+    mp3 = K.layers.MaxPooling2D((3, 3), strides=(2, 2),
+                                padding='same')(inc3b)
+    inc4a = inception_block(mp3, [192, 96, 208, 16, 48, 64])
+    inc4b = inception_block(inc4a, [160, 112, 224, 24, 64, 64])
+    inc4c = inception_block(inc4b, [128, 128, 256, 24, 64, 64])
+    inc4d = inception_block(inc4c, [112, 144, 288, 32, 64, 64])
+    inc4e = inception_block(inc4d, [256, 160, 320, 32, 128, 128])
+    mp4 = K.layers.MaxPooling2D((3, 3), strides=(2, 2),
+                                padding='same')(inc4e)
+    inc5a = inception_block(mp4, [256, 160, 320, 32, 128, 128])
+    inc5b = inception_block(inc5a, [384, 192, 384, 48, 128, 128])
+    avg1 = K.layers.AveragePooling2D((7, 7), strides=None)(inc5b)
+    drop1 = K.layers.Dropout(0.4)(avg1)
+    dense_1 = K.layers.Dense(1000, activation='softmax',
+                             kernel_initializer=init)(drop1)
+    model = K.models.Model(inputs=X, outputs=dense_1)
+    return(model)
