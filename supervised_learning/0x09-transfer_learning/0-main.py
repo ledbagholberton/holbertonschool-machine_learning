13aa078a4ca4ac886835a14fe9f1e24f76a94844
ledbagholberton <789@holbertonschool.com>
2020-04-10 23:32:43 +0000

    Multiple test

--- supervised_learning/0x09-transfer_learning/0-main.py
+++ supervised_learning/0x09-transfer_learning/0-main.py
@@ -10,4 +10,5 @@ K.learning_phase = K.backend.learning_phase
 _, (X, Y) = K.datasets.cifar10.load_data()
 X_p, Y_p = preprocess_data(X, Y)
 model = K.models.load_model('cifar10.h5')
-model.evaluate(X_p, Y_p, batch_size=128, verbose=1)
+a = model.evaluate(X_p, Y_p, batch_size=128, verbose=1)
+print(a)
--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -22,49 +22,59 @@ Y_p is a numpy.ndarray containing the preprocessed Y
 """
 import tensorflow.keras as K
 import numpy as np
-import h5py
 
 
 if __name__ == '__main__':
     (X, Y), _ = K.datasets.cifar10.load_data()
     X_train = K.applications.densenet.preprocess_input(X)
     y_train = K.utils.to_categorical(Y, 10)
-    model = K.applications.densenet.DenseNet121(
+    expand_x_train = np.fliplr(X_train)
+    X_train = np.concatenate([X_train, expand_x_train])
+    y_train = np.concatenate([y_train, y_train])
+    model = K.applications.vgg16.VGG16(
         weights='imagenet', 
         include_top=False,
         input_shape=(32,32,3),
-        classes=1000
+        classes=10
     )
-    for layer in model.layers:
+    """for layer in model.layers[:300]:
         layer.trainable = False
+    for layer in model.layers[300:]:
+        layer.trainable = True"""
     out_0 = model.layers[-1].output
-    out_1 = K.layers.Flatten()(out_0)
+    drop_1 = K.layers.Dropout(0.4)(out_0)
+    out_1 = K.layers.Flatten()(drop_1)
     out_2 = K.layers.Activation('relu')(out_1)
-    drop2 = K.layers.Dropout(rate=0.5)(out_2)
-    dense = K.layers.Dense(10, activation='softmax')(drop2)
-    model = K.models.Model(inputs=model.inputs, outputs=dense)
-
+    dense_1 = K.layers.Dense(256, activation='relu')(out_2)
+    drop_1 = K.layers.Dropout(0.4)(dense_1)
+    dense_3 = K.layers.Dense(10, activation='softmax')(drop_1)
+    model = K.models.Model(inputs=model.inputs, outputs=dense_3)
+    count = 0
+    for layer in model.layers[:15]:
+        layer.trainable = False
+    for layer in model.layers[16:]:
+        layer.trainable = True
+    model.summary()
+    print("Numero de capas", count)
     model.compile(
         loss='categorical_crossentropy',
-        optimizer='adam',
+        optimizer=K.optimizers.RMSprop(lr=1e-4),
         metrics=['accuracy']
     )
-    """checkpoint = K.callbacks.ModelCheckpoint(
-        'model.h5', 
-        monitor='val_acc', 
-        verbose=0, 
-        save_best_only=True, 
-        save_weights_only=False,
-        mode='auto'
-    )"""
-    model.summary()
-    # Train the model
+    lrr=K.callbacks.ReduceLROnPlateau(
+                monitor='acc',
+                factor=.01,
+                patience=2,
+                min_lr=1e-5)
+    nb = 1000
+    epochs = 5
     hist = model.fit(
         x=X_train,
         y=y_train,
-        batch_size=1,
-        epochs=1,
-        verbose=0
+        batch_size=nb,
+        epochs=epochs,
+        verbose=1,
+        callbacks=[lrr]
     )
     model.save('cifar10.h5')
 
@@ -73,5 +83,5 @@ def preprocess_data(X, Y):
     import tensorflow.keras as K
 
     x_p = K.applications.densenet.preprocess_input(X)
-    y_p = K.utils.to_categorical(Y)
+    y_p = K.utils.to_categorical(Y, 10)
     return (x_p, y_p)
