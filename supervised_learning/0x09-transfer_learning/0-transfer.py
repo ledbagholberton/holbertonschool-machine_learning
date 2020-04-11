f7e91ab5bfaf3271085ce16e939b1cf0b7ff26c0
ledbagholberton <789@holbertonschool.com>
2020-04-11 05:17:54 +0000

    95% train

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -26,12 +26,12 @@ import numpy as np
 
 if __name__ == '__main__':
     (X, Y), _ = K.datasets.cifar10.load_data()
-    X_train = K.applications.preprocess_input(X)
+    X_train = K.applications.densenet.preprocess_input(X)
     y_train = K.utils.to_categorical(Y, 10)
     expand_x_train = np.fliplr(X_train)
     X_train = np.concatenate([X_train, expand_x_train])
     y_train = np.concatenate([y_train, y_train])
-    model = K.applications.vgg16.VGG16(
+    model = K.applications.densenet.DenseNet121(
         weights='imagenet', 
         include_top=False,
         input_shape=(32,32,3),
@@ -42,39 +42,43 @@ if __name__ == '__main__':
     for layer in model.layers[300:]:
         layer.trainable = True """
     out_0 = model.layers[-1].output
-    drop_1 = K.layers.Dropout(0.4)(out_0)
+    drop_1 = K.layers.Dropout(0.3)(out_0)
     out_1 = K.layers.Flatten()(drop_1)
     out_2 = K.layers.Activation('relu')(out_1)
     dense_1 = K.layers.Dense(256, activation='relu')(out_2)
-    drop_1 = K.layers.Dropout(0.4)(dense_1)
+    drop_1 = K.layers.Dropout(0.3)(dense_1)
     dense_3 = K.layers.Dense(10, activation='softmax')(drop_1)
     model = K.models.Model(inputs=model.inputs, outputs=dense_3)
-    count = 0
-    for layer in model.layers[:15]:
+    for layer in model.layers[:80]:
         layer.trainable = False
-    for layer in model.layers[16:]:
+    for layer in model.layers[81:]:
         layer.trainable = True
     model.summary()
-    print("Numero de capas", count)
     model.compile(
         loss='categorical_crossentropy',
         optimizer=K.optimizers.RMSprop(lr=1e-4),
         metrics=['accuracy']
     )
     lrr=K.callbacks.ReduceLROnPlateau(
-                monitor='acc',
+                monitor='accuracy',
                 factor=.01,
                 patience=2,
                 min_lr=1e-5)
-    nb = 1000
-    epochs = 5
+    e_stop = K.callbacks.EarlyStopping(monitor='loss', patience=3)
+    checkpoint = K.callbacks.ModelCheckpoint(filepath='cifar10.h5',
+                                             monitor='val_accuracy',
+                                             verbose=1,
+                                             save_best_only=True,
+                                             mode='max')
+    nb = 32
+    epochs = 15
     hist = model.fit(
         x=X_train,
         y=y_train,
         batch_size=nb,
         epochs=epochs,
         verbose=1,
-        callbacks=[lrr]
+        callbacks=[lrr, e_stop, checkpoint]
     )
     model.save('cifar10.h5')
 
