d615ec63cdcd89ac96702fd7ed238a1d17faa3fc
ledbagholberton <789@holbertonschool.com>
2020-04-15 00:42:26 +0000

    Test

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
-""" Function Yolo Class Constructor
-Write a class Yolo that uses the Yolo v3 algorithm to perform
-object detection:
-
-model_path is the path to where a Darknet Keras model is stored
-classes_path is the path to where the list of class names used for the Darknet
-model, listed in order of index, can be found
-class_t is a float representing the box score threshold for the initial
-filtering step
-nms_t is a float representing the IOU threshold for non-max suppression
-anchors is a numpy.ndarray of shape (outputs, anchor_boxes, 2) containing
-all of the anchor boxes:
-outputs is the number of outputs (predictions) made by the Darknet model
-anchor_boxes is the number of anchor boxes used for each prediction
-2 => [anchor_box_width, anchor_box_height]
-Public instance attributes:
-model: the Darknet Keras model
-class_names: a list of the class names for the model
-class_t: the box score threshold for the initial filtering step
-nms_t: the IOU threshold for non-max suppression
-anchors: the anchor boxes
+""" Includes Method Process Outputs
+outputs is a list of numpy.ndarrays containing the predictions
+from the Darknet model for a single image:
+output shape (grid_height, grid_width, anchor_boxes, 4 + 1 + classes)
+grid_height & grid_width => the height and width of the grid used for
+the output
+anchor_boxes => the number of anchor boxes used
+4 => (t_x, t_y, t_w, t_h)
+1 => box_confidence
+classes => class probabilities for all classes
+image_size is a numpy.ndarray containing the image’s original
+size [image_height, image_width]
+Returns a tuple of (boxes, box_confidences, box_class_probs):
+boxes: a list of numpy.ndarrays of shape 
+(grid_height, grid_width, anchor_boxes, 4)
+containing the processed boundary boxes for each output, respectively:
+4 => (x1, y1, x2, y2)
+(x1, y1, x2, y2) should represent the boundary box relative to original image
+box_confidences: a list of numpy.ndarrays of shape (grid_height, grid_width,
+anchor_boxes, 1) containing the box confidences for each output, respectively
+box_class_probs: a list of numpy.ndarrays of shape (grid_height, grid_width,
+anchor_boxes, classes) containing the box’s class probabilities for each output
 """
 import tensorflow.keras as K
-
+import numpy as np
 
 class Yolo:
     """Class Yolo v3"""
@@ -40,3 +41,33 @@ class Yolo:
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
+
+    def process_outputs(self, outputs, image_size):
+        """Function process output"""
+        image_height = image_size[0]
+        image_width = image_size[1]
+        model_predict = K.predict(self)
+        grid_height, grid_width, anchor_boxes, dim_k = model_predict.shape
+        classes = dim_k - 4 -1 
+        boxes = np.zeros((grid_height, grid_width, anchor_boxes, 4))
+        boxes = model_predict[:,:,:,0:3]
+        for i in range(grid_width):
+            for j in range(grid_height):
+                for k in range(anchor_boxes):
+                    tx = model_predict[i,j,k,0]
+                    ty = model_predict[i,j,k,1]
+                    th = model_predict[i,j,k,2]
+                    tw = model_predict[i,j,k,3]
+                    x1 = ((image_width/grid_width)*i) + tx/2
+                    y1 = ((image_height/grid_height)*i) + ty/2
+                    x2 = x1 + tw
+                    y2 = y1 + th
+                    boxes[i,j,k,0] = model_predict[i,j,k,x1]
+                    boxes[i,j,k,1] = model_predict[i,j,k,y1]
+                    boxes[i,j,k,2] = model_predict[i,j,k,x2]
+                    boxes[i,j,k,3] = model_predict[i,j,k,y2]
+        box_confidences = np.zeros((grid_height, grid_width, anchor_boxes, 1))
+        box_confidences = model_predict[:,:,:,4]
+        box_class_probs = np.zeros((grid_height, grid_width, anchor_boxes, classes))
+        box_class_probs = model_predict[:,:,:,5:]
+        return((boxes, box_confidences, box_class_probs))
