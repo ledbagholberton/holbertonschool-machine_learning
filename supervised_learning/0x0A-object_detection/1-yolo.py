ec69f07b4e259449c15a31144cab1cbc2dcd0a11
ledbagholberton <789@holbertonschool.com>
2020-04-18 00:03:10 +0000

    Point 1 all changes

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -12,7 +12,7 @@ classes => class probabilities for all classes
 image_size is a numpy.ndarray containing the image’s original
 size [image_height, image_width]
 Returns a tuple of (boxes, box_confidences, box_class_probs):
-boxes: a list of numpy.ndarrays of shape 
+boxes: a list of numpy.ndarrays of shape
 (grid_height, grid_width, anchor_boxes, 4)
 containing the processed boundary boxes for each output, respectively:
 4 => (x1, y1, x2, y2)
@@ -24,6 +24,7 @@ anchor_boxes, classes) containing the box’s class probabilities for each outpu
 """
 import tensorflow.keras as K
 import numpy as np
+
 
 class Yolo:
     """Class Yolo v3"""
@@ -41,16 +42,12 @@ class Yolo:
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
-       
+
     def sigmoid(self, x):
-        return (1/(1 + np.exp(-x)))    
+        return (1/(1 + np.exp(-x)))
 
     def process_outputs(self, outputs, image_size):
-        """Function process output
-        def decode_netout(netout, anchors, obj_thresh, nms_thresh, net_h, net_w):
-        for i in range(len(yolos)):
-            boxes += decode_netout(yolos[i][0], anchors[i], obj_thresh, nms_thresh, net_h, net_w)
-        """
+        """Function process output"""
         boxes = []
         box_confidence = []
         box_class_probs = []
@@ -59,45 +56,31 @@ class Yolo:
         obj_tresh = 0.5
         nb_box = 3
         nb_class = 80
+        input_w = self.model.input.shape[1].value
+        input_h = self.model.input.shape[2].value
         for ii in range(len(outputs)):
             netout = outputs[ii]
             anchors = self.anchors[ii]
             grid_h, grid_w = netout.shape[:2]
-            netout = netout.reshape((grid_h, grid_w, nb_box, -1))
-            netout[..., :1]  = self.sigmoid(netout[..., :1])
-            netout[..., 4:]  = self.sigmoid(netout[..., 4:])
-            for i in range(grid_h):
-                for j in range(grid_w):
-                    row = i
-                    col = j
+            netout[..., :2] = self.sigmoid(netout[..., :2])
+            netout[..., 4:] = self.sigmoid(netout[..., 4:])
+            np_bx = netout[..., :4]
+            for i in range(grid_w):
+                for j in range(grid_h):
                     for b in range(nb_box):
-                        confidence = netout[int(row)][int(col)][b][4]
-                        x, y, w, h = netout[int(row)][int(col)][b][:4]
-                        x = (col + x) / grid_w # center position, unit: image width
-                        y = (row + y) / grid_h # center position, unit: image height
-                        w = anchors[b][0] * np.exp(w) / net_w # unit: image width
-                        h = anchors[b][1] * np.exp(h) / net_h # unit: image height  
-                        # last elements are class probabilities
-                        classes = netout[int(row)][col][b][5:]
-                        x1 = x-w/2
-                        x2 = x+w/2
-                        y1 = y-h/2
-                        y2 = y+h/2
-                        print("x1:", x1)
-                        print("x2:", x2)
-                        print("y1:", y1)
-                        print("y2:", y2)
-            np_bx = np.ndarray((grid_h, grid_w, nb_box, 4))
-            np_bx = netout[...,:4]
-            np_bx[...,:4] = x1, x2, y1, y2
+                        x, y, w, h = netout[i][j][b][:4]
+                        x = (x + i) / grid_w
+                        y = (y + j) / grid_h
+                        w = (anchors[b][0] * np.exp(w)) / input_w
+                        h = (anchors[b][1] * np.exp(h)) / input_h
+                        x1 = (x - w/2) * net_w
+                        y1 = (y - h/2) * net_h
+                        x2 = (x + w/2) * net_w
+                        y2 = (y + h/2) * net_h
+                        np_bx[i, j, b, 0:4] = x1, y1, x2, y2
             boxes.append(np_bx)
-            np_bx_conf = np.ndarray((grid_h, grid_w, nb_box, 1))
-            np_bx_conf = netout[...,4]
-            np_bx_conf[...,0]=confidence
+            np_bx_conf = netout[..., 4:5]
             box_confidence.append(np_bx_conf)
-            np_bx_clss = np.ndarray((grid_h, grid_w, nb_box, nb_class)) 
-            np_bx_clss = netout[...,5:]
-            np_bx_clss[...,:] = classes
+            np_bx_clss = netout[..., 5:]
             box_class_probs.append(np_bx_clss)
         return (boxes, box_confidence, box_class_probs)
-
