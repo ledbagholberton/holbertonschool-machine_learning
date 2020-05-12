f8b31e80c8b43f1bba066265e8a12d3913e2c4d8
ledbagholberton <789@holbertonschool.com>
2020-05-12 04:26:02 +0000

    fix requirement

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -50,7 +50,7 @@ class MultiNormal:
         if not(isinstance(x, np.ndarray)):
             raise TypeError("x must be a numpy.ndarray")
         cov = self.cov
-        if (x.shape is not 2 and x.shape[1] is not 1
+        if (len(x.shape) is not 2 and x.shape[1] is not 1
                 and x.shape[0] is not cov.shape[0]):
             raise TypeError("x mush have the shape ({d}, 1)".
                             format(cov.shape[0]))
