9718113269806307c22b587a7f4c33a963a40710
ledbagholberton <789@holbertonschool.com>
2020-05-16 00:07:38 +0000

    Change the formula for euclidean space

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -21,7 +21,7 @@ import numpy as np
 def P_init(X, perplexity):
     """FUnction Init """
     n, d = X.shape
-    sum_X = np.sum(np.square(X), 1, keepdims=True)
+    sum_X = np.sum(np.square(X), 1)
     D = np.add(np.add(-2 * np.matmul(X, X.T), sum_X).T, sum_X)
     P = np.zeros((n, n))
     betas = np.ones((n, 1))
