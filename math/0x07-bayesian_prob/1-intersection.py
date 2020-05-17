615acc4cfd22c54a7165b6bb515ebfe8d942d6a8
ledbagholberton <789@holbertonschool.com>
2020-05-17 02:52:05 +0000

    Fix issue related shape of variable not array

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -38,14 +38,16 @@ def factorial(n):
 
 def intersection(x, n, P, Pr):
     """Function likelihood"""
-    if type(n) is not int or n < 0:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
     if type(x) is not int or x < 0:
         msg1 = "x must be an integer that is greater than or equal to 0"
         raise ValueError(msg1)
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) and len(P.shape) is not 1:
+    if not isinstance(P, np.ndarray):
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if len(P.shape) is not 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if np.amax(P) > 1 or np.amin(P) < 0:
         raise ValueError("All values in P must be in the range [0, 1]")
