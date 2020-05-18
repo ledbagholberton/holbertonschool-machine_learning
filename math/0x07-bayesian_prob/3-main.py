1065a76b9790d7da96c34d5118861a0e9ea2e84b
ledbagholberton <789@holbertonschool.com>
2020-05-18 21:03:21 +0000

    Point 100

--- math/0x07-bayesian_prob/3-main.py
+++ math/0x07-bayesian_prob/3-main.py
@@ -2,8 +2,12 @@
 
 if __name__ == '__main__':
     import numpy as np
+    import matplotlib.pyplot as plt
     posterior = __import__('3-posterior').posterior
 
-    P = np.linspace(0, 1, 101)
-    Pr = np.ones(101) / 101
-    print(posterior(26, 130, P, Pr))
+    P = np.linspace(0, 1, 11)
+    Pr = np.ones(11) / 11
+    a = posterior(26, 130, P, Pr)
+    print(a)
+    plt.plot(a)
+    plt.show()
