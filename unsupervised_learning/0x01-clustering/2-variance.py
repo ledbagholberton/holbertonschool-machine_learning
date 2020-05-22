1567d0d1dd4650def46608341179792cbbc74927
ledbagholberton <789@holbertonschool.com>
2020-05-22 17:38:46 +0000

    With except in point 2

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -15,10 +15,13 @@ def variance(X, C):
     """Intracluster variance"""
     if not verify(X, C):
         return None
-    distances = np.sqrt(((X - C[:, np.newaxis])**2).sum(axis=-1))
-    closest = np.min(distances, axis=0)
-    sum = np.sum(np.power(closest, 2))
-    return sum
+    try:
+        distances = np.sqrt(((X - C[:, np.newaxis])**2).sum(axis=-1))
+        closest = np.min(distances, axis=0)
+        sum = np.sum(np.power(closest, 2))
+        return sum
+    except Exception:
+        return None
 
 
 def verify(X, C):
