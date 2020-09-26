249b19a296cf0ac9d4f5977bf7f7fd5fcda5fa8e
ledbagholberton <789@holbertonschool.com>
2020-09-26 00:17:52 -0500

    Point 13

--- pipeline/0x00-pandas/13-analyze.py
+++ pipeline/0x00-pandas/13-analyze.py
@@ -5,6 +5,6 @@ from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-stats = # YOUR CODE HERE
+stats = df.describe()
 
 print(stats)
