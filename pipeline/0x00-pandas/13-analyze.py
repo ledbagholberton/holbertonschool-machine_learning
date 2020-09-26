c961a331d651876c48010a080b60bca9baf5cc9e
ledbagholberton <789@holbertonschool.com>
2020-09-26 00:21:03 -0500

    Point 13 without Timestamp

--- pipeline/0x00-pandas/13-analyze.py
+++ pipeline/0x00-pandas/13-analyze.py
@@ -4,7 +4,7 @@ import pandas as pd
 from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
-stats = df.describe()
+df_1 = df.drop(["Timestamp"], axis=1)
+stats = df_1.describe()
 
 print(stats)
