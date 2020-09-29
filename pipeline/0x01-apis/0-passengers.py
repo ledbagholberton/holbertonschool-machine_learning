6492f1e8f77057a98fc51100f04bf3cf724d1871
ledbagholberton <789@holbertonschool.com>
2020-09-28 19:30:13 -0500

    point 0 & 1

---
+++ pipeline/0x01-apis/0-passengers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+By using the Swapi API, create a method that returns the list
+of ships that can hold a given number of passengers:
+Don’t forget the pagination
+If no ship available, return an empty list.
+"""
+import requests
+
+
+def availableShips(passengerCount):
+    """ 
+    Function availableShips
+    """
+    url = "https://swapi-api.hbtn.io/api/starships/"
+    transport = []
+    while url is not None:
+        r = requests.get(url)
+        datos = r.json()["results"]
+        for ship in datos:
+            a = ship["passengers"]
+            a = a.replace(',', '')
+            if a.isnumeric() and int(a) >= passengerCount:
+                transport.append(ship["name"])
+        url = r.json()["next"]
+    return transport
---
+++ pipeline/0x01-apis/1-sentience.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+By using the Swapi API, create a method that returns
+the list of names of the home planets of all sentient species.
+Don’t forget the pagination
+"""
+import requests
+
+
+def sentientPlanets():
+    """ 
+    Function availableShips
+    """
+    url = "https://swapi-api.hbtn.io/api/species/"
+    species = []
+    planets = []
+    while url is not None:
+        r = requests.get(url)
+        datos = r.json()["results"]
+        for iter in datos:
+            if (iter["classification"] == "sentient"
+                    or iter["designation"] == "sentient"):
+                new_url = iter["homeworld"]
+                new_r = requests.get(new_url)
+                new_datos = new_r.json()["results"]
+                print(new_datos)
+                planets.append(new_datos)
+        url = r.json()["next"]
+    return planets
