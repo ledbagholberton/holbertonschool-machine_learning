d189493f5e5dc4a3560f248adac647ffa4ae2f23
ledbagholberton <789@holbertonschool.com>
2020-10-02 23:04:58 -0500

    MongoDB files 22-30

--- pipeline/0x02-databases/9-no_genre.sql
+++ pipeline/0x02-databases/9-no_genre.sql
@@ -6,4 +6,4 @@ LEFT OUTER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT OUTER JOIN tv_genres 
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.genre_id IS NULL
+WHERE tv_show_genres.genre_id IS NULL;
